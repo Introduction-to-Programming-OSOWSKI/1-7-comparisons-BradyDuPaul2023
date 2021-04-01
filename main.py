bd0ee3b2a3dda07bf9497159bf21107807926ac6
Dupaul Braedon <816947@stma.k12.mn.us>
2021-04-01 09:53:00 -0500

    Comparisons try 1

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x, y):
+    if abs(x) > abs(y):
+        print("True")
+    else:
+        print("False")
+print(greaterThan(10, 8))
+def lessThan(a, b):
+    if abs(a) < abs(b):
+        print("True")
+    else:
+        print("False")
+print(lessThan(8, 8))
+def equalTo(q, w):
+    if abs(q) == abs(w):
+        print("True")
+    else:
+        print("false")
